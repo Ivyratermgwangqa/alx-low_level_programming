b0d6a5eaf85ddd653e324a7c0e06e3f432334b4b
Ivyratermgwangqa <ivyratermgwangqa@gmail.com>
2023-02-16 18:09:49 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main -execute a code ........display Programming is like building a multilingual puzzle with puts
+ * Return:0 in all the times(success)
+ */ 
+int main(void) {
+  puts("Programming is like building a multilingual puzzle");
+  return(0);
+}
