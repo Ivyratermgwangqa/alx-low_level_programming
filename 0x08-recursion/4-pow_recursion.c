a03a31591a4382f17398e40c3c845565960f0d35
Ivyratermgwangqa <ivyratermgwangqa@gmail.com>
2023-06-08 03:42:15 -0700

    4-pow_recursion.c

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _pow_recursion - Calculates the power of x and y using recursion.
+ *
+ * @x: The base number.
+ * @y: The exponent.
+ * Return: the power of exponent
+ */
+
+int _pow_recursion(int x, int y)
+{
+if (y < 0)
+return (-1)
+if (y == 0)
+return (1);
+return (x * _pow_recursion(x, y - 1));
+}
