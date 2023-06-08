b9720f93da9100081adc82a59a59a2c1bb95869d
Ivyratermgwangqa <ivyratermgwangqa@gmail.com>
2023-06-08 02:01:11 -0700

    0-puts_recursion.c

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -3,7 +3,6 @@
 /**
  * _puts_recursion - function puts();
  * @s: input
- * puts - function puts();
  * Return: Always 0
  */
 void _puts_recursion(char *s)
@@ -17,9 +16,4 @@ else
 putchar(*s);
 _puts_recursion(s + 1);
 }
-}
-void puts(char *s)
-{
-_puts_recursion(s);
-putchar('\n');
 }
