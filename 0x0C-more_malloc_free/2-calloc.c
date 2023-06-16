87913774c15f0c79452985baf8a308588cb8c8bd
Ivyratermgwangqa <ivyratermgwangqa@gmail.com>
2023-06-16 10:39:24 -0700

    malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,24 +1,6 @@
 #include "main.h"
 #include <string.h>
 #include <stdio.h>
-
-/**
- * _memset - Fills a block of memory with a specific value.
- * @s: Pointer to the memory block.
- * @b: Value to be set.
- * @n: Number of bytes to be set.
- *
- * Return: Pointer to the memory block.
- */
-char *_memset(char *s, char b, unsigned int n)
-{
-unsigned int i;
-for (i = 0; i < n; i++)
-{
-s[i] = b;
-}
-return (s);
-}
 /**
  * _calloc - Allocates memory for an array and initializes it to zero.
  * @nmemb: The number of elements in the array.
@@ -28,7 +10,7 @@ return (s);
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-void *ptr;
+char *ptr;
 unsigned int i;
 if (nmemb == 0 || size == 0)
 return (NULL);
@@ -36,7 +18,6 @@ ptr = malloc(nmemb * size);
 if (ptr == NULL)
 return (NULL);
 for (i = 0; i < nmemb * size; i++)
-*(char *)ptr++ = 0;
-_memset(ptr, 0, nmemb * size);
+ptr[i] = 0;
 return (ptr);
 }
