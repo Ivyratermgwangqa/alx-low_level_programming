07526c1df6048cca472e6437be581b10a6150bdf
Ivyratermgwangqa <ivyratermgwangqa@gmail.com>
2023-06-19 16:40:34 -0700

    preprocessor,sum,function like macro,main,pi and objects like macro

---
+++ 0x0D-preprocessor/1-pi.h
@@ -0,0 +1,5 @@
+#ifndef _MAIN_H_
+#define _MAIN_H_
+#ifndef PI
+#define PI 3.14159265359
+#endif
---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - Print the name of the file compiled from,followed by a new line
+ *
+ * Return: 0
+ */
+int main(void)
+{
+printf("File: %s\n", __FILE__);
+return (0);
+}
