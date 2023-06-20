7a139598a56ae283c4097740747943b846f764d3
Ivyratermgwangqa <ivyratermgwangqa@gmail.com>
2023-06-19 18:52:05 -0700

    structres and typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,6 +14,7 @@ struct dog {
     char *owner;
 };
 typedef struct dog dog_t;
+int _putchar(char);
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
