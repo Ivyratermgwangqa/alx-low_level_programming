b99c4356a2dc2d797690fa280eb0e3f1b8897eed
Ivyratermgwangqa <ivyratermgwangqa@gmail.com>
2023-07-01 18:31:57 -0700

    update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 /**
- * before_main - prints context before the main function is executed.
- * Return: void
+ * __attribute__((constructor)) before_main - prints before main.
+ * Return: Always 0.
  */
 int __attribute__((constructor)) before_main()
 {
 	printf("You're beat! and yet, you must allow,\n");
 	printf("I bore my house upon my back !\n");
-	return(0);
+	return (0);
 }
