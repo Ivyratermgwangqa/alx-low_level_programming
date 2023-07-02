d502da534e579afc2f846515471009238862e556
Ivyratermgwangqa <ivyratermgwangqa@gmail.com>
2023-07-01 18:06:03 -0700

    update

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,10 @@
+#include "lists.h"
+/**
+ * before_main - prints context before the main function is executed.
+ * Return: void
+ */
+void before_main(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
