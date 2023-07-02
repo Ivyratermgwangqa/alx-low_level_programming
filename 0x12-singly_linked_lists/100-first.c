bb036b80e1566c911f999060536d059879f43dab
Ivyratermgwangqa <ivyratermgwangqa@gmail.com>
2023-07-01 18:21:20 -0700

    update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -3,8 +3,8 @@
  * before_main - prints context before the main function is executed.
  * Return: void
  */
-void before_main(void)
+void __attribute__((constructor)) before_main()
 {
 	printf("You're beat! and yet, you must allow,\n");
-	printf("I bore my house upon my back!\n");
+	printf("I bore my house upon my back !\n");
 }
