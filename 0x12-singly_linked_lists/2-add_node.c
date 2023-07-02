d30cb97bde720ff2263a6266fc0f6e6294970a7f
Ivyratermgwangqa <ivyratermgwangqa@gmail.com>
2023-07-01 17:57:01 -0700

    update

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -9,15 +9,15 @@
 list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new;
-	size_t num_node;
+	size_t num_node = 0;
 
 	new = malloc(sizeof(list_t));
 	if (new == NULL)
 		return (NULL);
-
 	new->str = strdup(str);
 
-	for (num_node = 0; str[num_node]; num_node++);
+	while (str[num_node])
+		num_node++;
 
 	new->len = num_node;
 	new->next = *head;
