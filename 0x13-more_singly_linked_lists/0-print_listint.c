6085427aa809ef49b994230808a960b338cc8bca
Ivyratermgwangqa <ivyratermgwangqa@gmail.com>
2023-07-03 19:46:02 -0700

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * print_listint - function that prints all the elements of a listint_t list.
+ * print_listint - function that prints all the elements of a listint_t.
  * @h: data type pointer of struct
  * Return: the number of nodes
  */
