da7f64202c44b7d388d51e3906b968a54752e16a
Ivyratermgwangqa <ivyratermgwangqa@gmail.com>
2023-09-13 20:00:46 -0700

    update

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -4,7 +4,7 @@
  * @ht: A pointer to the hash table
  * @key: The key, string
  * @value: The value corresponding to a key
- * Return: 0 if fails, 1 if successful
+ * Return: 0 if fails, 1 if success
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
@@ -14,23 +14,24 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 
 	if (ht == NULL || key == NULL || *key == '\0' || value == NULL)
 		return (0);
+
 	copy_value = strdup(value);
 	if (copy_value == NULL)
 		return (0);
+
 	index = key_index((const unsigned char *)key, ht->size);
 	current_index = index;
 
-	while (ht->array[current_index] != NULL)
+	for (current_index = index; ht->array[current_index] != NULL; ++current_index)
 	{
 		if (strcmp(ht->array[current_index]->key, key) == 0)
 		{
 			free(ht->array[current_index]->value);
 			ht->array[current_index]->value = copy_value;
-			free(copy_value);
 			return (1);
 		}
-		current_index++;
 	}
+
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 	{
@@ -40,12 +41,12 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node->key = strdup(key);
 	if (new_node->key == NULL)
 	{
-		free(copy_value);
 		free(new_node);
 		return (0);
 	}
 	new_node->value = copy_value;
 	new_node->next = ht->array[index];
 	ht->array[index] = new_node;
+
 	return (1);
 }
