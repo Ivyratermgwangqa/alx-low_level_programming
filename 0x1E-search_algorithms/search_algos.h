ca41fc070178088fa240834e0630abb87da77526
root <Ivyratermgwangqa>
2024-02-08 14:10:49 -0800

    update

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -15,10 +15,10 @@
  */
 typedef struct listint_s
 {
-    int n;
-    size_t index;
-    struct listint_s *next;
-} listint_t;
+	int n;
+	size_t index;
+	struct listint_s *next;
+}listint_t;
 
 /**
  * struct skiplist_s - Singly linked list with an express lane
@@ -31,11 +31,11 @@ typedef struct listint_s
  */
 typedef struct skiplist_s
 {
-    int n;
-    size_t index;
-    struct skiplist_s *next;
-    struct skiplist_s *express;
-} skiplist_t;
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+}skiplist_t;
 
 /* Function Prototypes */
 int linear_search(int *array, size_t size, int value);
