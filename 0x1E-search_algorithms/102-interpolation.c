3c303e72b36779e51d37ac2dfd359a4223a6cff1
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-02-09 00:56:23 +0200

    Update 102-interpolation.c

--- 0x1E-search_algorithms/102-interpolation.c
+++ 0x1E-search_algorithms/102-interpolation.c
@@ -11,27 +11,33 @@
 */
 int interpolation_search(int *array, size_t size, int value)
 {
-size_t low = 0, high = size - 1;
-size_t pos;
+    size_t low = 0, high = size - 1;
+    size_t pos;
 
-if (array == NULL || size == 0)
-return (-1);
+    if (array == NULL || size == 0)
+        return (-1);
 
-while (low <= high && value >= array[low] && value <= array[high])
-{
-pos = low + (((double)(high - low) / (array[high] - array[low])) *
-(value - array[low]));
-printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
+    double ratio = (double)(high - low) / (array[high] - array[low]);
+    double distance = value - array[low];
+    pos = low + (ratio * distance);
 
-if (array[pos] == value)
-return (pos);
+    while (low <= high && value >= array[low] && value <= array[high])
+    {
+        printf("Value checked array[%lu] = [%d]\n", pos, array[pos]);
 
-if (array[pos] < value)
-low = pos + 1;
-else
-high = pos - 1;
-}
+        if (array[pos] == value)
+            return (pos);
 
-printf("Value checked array[%lu] is out of range\n", pos);
-return (-1);
+        if (array[pos] < value)
+            low = pos + 1;
+        else
+            high = pos - 1;
+
+        ratio = (double)(high - low) / (array[high] - array[low]);
+        distance = value - array[low];
+        pos = low + (ratio * distance);
+    }
+
+    printf("Value checked array[%lu] is out of range\n", pos);
+    return (-1);
 }
