739a7b25ad2a79b02d711dac79dc2c0390261f44
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-02-09 00:28:59 +0200

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -39,7 +39,7 @@ typedef struct skiplist_s
 
 /* Function Prototypes */
 int linear_search(int *array, size_t size, int value);
-int binary_search(int *array, size_t left, size_t right, int value);
+int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
