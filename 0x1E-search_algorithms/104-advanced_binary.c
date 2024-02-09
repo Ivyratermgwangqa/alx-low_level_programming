e3cc1798976a4ef258af563667c7b33c3d2d6d46
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-02-09 14:21:03 +0200

    Update 104-advanced_binary.c

--- 0x1E-search_algorithms/104-advanced_binary.c
+++ 0x1E-search_algorithms/104-advanced_binary.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 #include "search_algos.h"
+int recurse_helper(int *array, size_t left,
+size_t right, int value, ssize_t *match);
 
 /**
- * recursive_advanced_binary - Searches for value in sorted array of integers
+ * recurse_helper - Searches for value in sorted array of integers
  *                             using Advanced Binary search algo recursively
  * @array: Pointer to the first element of the array to search in
  * @left: Index representing the left bound of the search range
@@ -10,35 +12,37 @@
  * @value: Value to search for
  * Return: Index where value is located, or -1 if not found
  */
-int recursive_advanced_binary(int *array, size_t left, size_t right, int value)
+int recurse_helper(int *array, size_t left,
+size_t right, int value, ssize_t *match)
 {
-	size_t i, mid;
-	
-	if (array == NULL || left > right)
-		return -1;
-	
-	mid = left + (right - left) / 2;
-	
-	printf("Searching in array: ");
-	for (i = left; i <= right; i++)
-	{
-		printf("%d", array[i]);
-		if (i < right)
-			printf(", ");
-	}
+	size_t i = left, mid;
+
+	if (left > right)
+		return (*match);
+
+	/* print search progress */
+	printf("Searching in array: %d", array[i++]);
+	while (i <= right)
+		printf(", %d", array[i++]);
 	printf("\n");
-	
+
+	/* calculate mid */
+	mid = left + ((right - left) / 2);
+
+	/* check if mid is value */
 	if (array[mid] == value)
 	{
-		if (mid == 0 || array[mid - 1] != value)
-			return mid;
-		else
-			return recursive_advanced_binary(array, left, mid, value);
+		*match = mid;
+		if (right - left > 1)
+			mid++;
 	}
-	else if (array[mid] < value)
-		return recursive_advanced_binary(array, mid + 1, right, value);
+	else if (array[mid] < value) /* search right */
+		return (recurse_helper(array, mid + 1, right, value, match));
+
+	if (mid != 0)
+		return (recurse_helper(array, left, mid - 1, value, match));
 	else
-		return recursive_advanced_binary(array, left, mid - 1, value);
+		return (*match);
 }
 /**
  * advanced_binary - Searches for a value in a sorted array of integers
@@ -50,7 +54,10 @@ int recursive_advanced_binary(int *array, size_t left, size_t right, int value)
  */
 int advanced_binary(int *array, size_t size, int value)
 {
-	if (array == NULL || size == 0)
-		return -1;
-	return recursive_advanced_binary(array, 0, size - 1, value);
+	ssize_t match = -1;
+
+	if (array == NULL)
+		return (-1);
+
+	return (recurse_helper(array, 0, size - 1, value, &match));
 }
