0473b610a8d008b6bab2802cbd30d998e348a440
Lerato Mgwangqa <122816970+Ivyratermgwangqa@users.noreply.github.com>
2024-02-09 14:16:58 +0200

    Update search_algos.h

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -47,4 +47,9 @@ int advanced_binary(int *array, size_t size, int value);
 int recursive_advanced_binary(int *array, size_t left, size_t right, int value);
 listint_t *jump_list(listint_t *list, size_t size, int value);
 skiplist_t *linear_skip(skiplist_t *list, int value);
+
+/* UTILITIES */
+listint_t *create_list(int *array, size_t size);
+void free_list(listint_t *list);
+void print_list(const listint_t *list);
 #endif /* SEARCH_ALGOS_H */
